97da23cd674c0cd50f31ddd38e6c564472a2e451
Santiago Betancur <santi-c-b@outlook.com>
2018-11-25 16:42:29 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
-  return (if num == nil then
-            nil
-          elsif num % 3 == 0 then
+  return (if num % 3 == 0 then
             if num % 5 == 0 then
               "FizzBuzz"
             else
