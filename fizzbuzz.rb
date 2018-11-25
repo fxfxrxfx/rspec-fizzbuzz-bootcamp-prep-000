80cdd9d2c6664e101081b364ec8140c8171a2981
Santiago Betancur <santi-c-b@outlook.com>
2018-11-25 16:41:59 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,7 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
   return (if num == nil then
+            nil
           elsif num % 3 == 0 then
             if num % 5 == 0 then
               "FizzBuzz"
